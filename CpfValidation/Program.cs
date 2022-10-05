75cf4e1cb9b7970da02823bdc81327a1937e5847
Guilherme <guilherme.bandeira@clearsale.com.br>
2022-10-05 19:25:49 -0300

    First commit

---
+++ CpfValidation/Program.cs
@@ -0,0 +1,19 @@
+﻿using CpfValidation.Service;
+
+
+while (true)
+{
+    Console.WriteLine("\nInput cpf:");
+    var input = Console.ReadLine();
+    var result = Validation.IsCpfValid(input!);
+    if (result)
+    {
+        Console.WriteLine("Cpf is valid");
+    }
+    else
+    {
+        Console.WriteLine("Cpf is invalid");
+    }
+}
+
+
